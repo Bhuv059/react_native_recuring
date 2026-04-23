d19a92aef30bee0213397ce99a60760accaea600
Bhuv <bhuv059@gmail.com>
2026-04-23 11:48:30 +0200

    impelement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import {Link} from "expo-router";
 import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 import {styled} from "nativewind"
@@ -7,16 +7,12 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <View className="flex-1 items-center justify-center bg-background">
-                <Text className="text-xl font-bold text-success">
-                    Welcome to Nativewind!
+                <Text className="text-7xl font-sans-extrabold text-success">
+                    Home
                 </Text>
-                <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
-                <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign in</Link>
-                <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up</Link>
-                <Link href="/subscriptions/spotify" >Spotify Subscription</Link>
-                <Link href={{pathname: "/subscriptions/[id]", params:{id:"claude"},}} >Claude Max Subscription</Link>
-            </View>
+                <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Onboarding</Link>
+                <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Sign in</Link>
+                <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Sign Up</Link>
         </SafeAreaView>
     );
 }
